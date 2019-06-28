cf118f69f2da0ab448ba303384c9a63eef91b2c8
root <root@dev.quantguard>
2019-06-28 17:41:30 +0200

    cleanup

--- user.go
+++ user.go
@@ -54,42 +54,4 @@ func (c *Session) DeleteUsers(UserIDs ...string) (UserIds []string, err error) {
 // UserUpdateParams struct represents the Zabbix basic parameters for
 // updating the User by Zabbix API
 /*type UserUpdateParams struct {
-    User
-    Interfaces      UserInterfaces `json:"interfaces,omitempty"`
-    Templates       Templates      `json:"templates,omitempty"`
-    UnlinkTemplates Templates      `json:"templates,omitempty"`
-    // Inventory Inventory `json:"inventory,omitempty"`
 }
-
-// GetUsers queries the Zabbix API for Users matching the given search
-// parameters.
-//
-// ErrNotFound is returned if the search result set is empty.
-// An error is returned if a transport, parsing or API error occurs.
-//
-// https://www.zabbix.com/documentation/3.4/manual/api/reference/User/get
-func (c *Session) GetUsers(params UserGetParams) ([]User, error) {
-	Users := make([]jUser, 0)
-	err := c.Get("user.get", params, &Users)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(Users) == 0 {
-		return nil, ErrNotFound
-	}
-
-	// map JSON Events to Go Events
-	out := make([]User, len(Users))
-	for i, jUser := range Users {
-		User, err := jUser.User()
-		if err != nil {
-			return nil, fmt.Errorf("Error mapping User %d in response: %v", i, err)
-		}
-
-		out[i] = *User
-	}
-
-	return out, nil
-}*/
-
